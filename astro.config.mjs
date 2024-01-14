c80c0adfe3d714877b1505b4207868732f456ca2
Sean Beard <sean61793@my.uri.edu>
2024-01-14 14:15:05 -0700

    chore(config): use htmx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import node from "@astrojs/node";
+import htmx from "astro-htmx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,5 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  integrations: [htmx()],
 });
